8070c5926bc2d410c5ce396b152ba0ce400ca0b7
natxf <157922167+natxf@users.noreply.github.com>
2024-10-10 07:33:22 +0000

    zadanie 1.1

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 (function () {
-  //TODO
+  (function () { 
+
+    var ex1_button=document.getElementById('ex1_button');  
+
+    var ex1_content=document.getElementById('ex1_content');  
+
+    ex1_button.onclick=function(){   
+
+      var tabela=[]   
+
+      for(var i=0;i<=9;i++)   
+
+      {     
+
+        tabela.push(i);   
+
+      }     
+
+      ex1_content.innerHTML=tabela.toString(); 
+
+    } 
+
+  })();
 })();
